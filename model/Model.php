34a31aa52964c3b360b5da2d636d7eeb28915016
Johana Infesta <johana.infesta@gmail.com>
2018-09-23 15:49:27 -0300

    ajustando model.php

--- model/Model.php
+++ model/Model.php
@@ -7,11 +7,11 @@ class Model
   const DB_PASS = '';
 
   protected $db;
-  
+
   function __construct()
   {
     try {
-      $this->db = new PDO('mysql:host=localhost;dbname=dimensionmanga;charset=utf8', self::DB_USER, self::DB_PASS);
+      $this->db = new PDO('mysql:host=localhost;dbname=theLibrary;charset=utf8', self::DB_USER, self::DB_PASS);
     } catch (PDOException $e) {
       $this->install();
     } catch (Exception $e) {
@@ -23,8 +23,8 @@ class Model
     try {
       $this->db = new PDO('mysql:host=localhost', self::DB_USER, self::DB_PASS);
       $this->db->exec('CREATE DATABASE IF NOT EXISTS dimensionmanga');
-      $this->db->exec('USE dimensionmanga');
-      $sql = file_get_contents('./install/dimensionmanga.sql');
+      $this->db->exec('USE theLibrary');
+      $sql = file_get_contents('./install/theLibrary.sql');
       $this->db->exec($sql);
     } catch(Exception $e) {
       echo $e->getMessage();
