2107e8179c2ffb429fa0d6015899ad6e18396ab5
Johana Infesta <johana.infesta@gmail.com>
2018-10-18 14:01:05 -0300

    ¬

--- config/ConfigApp.php
+++ config/ConfigApp.php
@@ -2,25 +2,26 @@
 
 class ConfigApp
 {
-    public static $ACTION = 'action';
-    public static $PARAMS = 'params';
-    public static $ACTIONS = [
-        ''=> 'IndexController#index',
-        //Configuración para url's items
+  public static $ACTION = 'action';
+  public static $PARAMS = 'params';
+  public static $ACTIONS = [
+    ''=> 'IndexController#index',
+    //Configuración para url's items
+    
+    //Configuración para url's categorias
+    'listaLibros' => 'LibrosController#index',
 
-        //Configuración para url's categorias
-
-        //Configuración para url's login, logout y verificaciones
-        //'login' => 'LoginController#index',
-        //'registro' => 'LoginController#registro',
-        // 'logout' => 'LoginController#destroy',
-        // 'verificarUsuario' => 'LoginController#verify',
-        // 'crearUsuario' => 'LoginController#create',
-        // 'eliminarUsuario' => 'LoginController#delete',
-        // 'adminUsers' => 'LoginController#listaUsers',
-        // 'permisoSuperUser' => 'LoginController#superUser'
-    ];
+    //Configuración para url's login, logout y verificaciones
+    //'login' => 'LoginController#index',
+    //'registro' => 'LoginController#registro',
+    // 'logout' => 'LoginController#destroy',
+    // 'verificarUsuario' => 'LoginController#verify',
+    // 'crearUsuario' => 'LoginController#create',
+    // 'eliminarUsuario' => 'LoginController#delete',
+    // 'adminUsers' => 'LoginController#listaUsers',
+    // 'permisoSuperUser' => 'LoginController#superUser'
+  ];
 
 }
 
- ?>
+?>
--- controller/autorController.php
+++ controller/autorController.php
@@ -1,5 +1,23 @@
 <?php
+include_once('model/AutoresModel.php');
+include_once('model/LibrosModel.php');
+include_once('view/AutoresView.php');
+
+class AutorController extends Controller
+{
+
+  function __construct()
+  {
+    $this->view = new AutoresView();
+    $this->model = new AutoresModel();
+    $this->mangaModel = new LibrosModel();
+  }
+
+  public function index()
+  {
+    
+  }
 
 
-
- ?>
+}
+?>
