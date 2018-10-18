139638bf1a3a4959e08b3acf7606c335abe4f441
Antonela Nespoli <antonela.nespoli@gmail.com>
2018-10-18 17:07:59 -0300

    se modifican los nombres de los metodos

--- model/AuthorModel.php
+++ model/AuthorModel.php
@@ -2,29 +2,29 @@
 class AuthorModel extends Model
 {
 
-  function getAutores(){
+  function getAuthors(){
     $sentencia = $this->db->prepare( "SELECT * FROM author");
     $sentencia->execute();
     return $sentencia->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  function getAutor($id_author){
+  function getAuthor($id_author){
     $sentencia = $this->db->prepare("SELECT * FROM author WHERE id_author = ?");
     $sentencia->execute([$id_author]);
     return $sentencia->fetch();
   }
 
-  function guardarAutor($name, $surname, $nationality, $biography){
+  function saveAuthor($name, $surname, $nationality, $biography){
     $sentencia=$this->db->prepare("INSERT INTO author(name, surname, nationality, biography) VALUES(?)");
     $sentencia->execute([$name, $surname, $nationality, $biography]);
   }
 
-  function editarAutor($id_author, $name, $surname, $nationality, $biography){
+  function editAuthor($id_author, $name, $surname, $nationality, $biography){
     $sentencia = $this->db->prepare("UPDATE author SET (name = ?, surname = ?, nationality = ?, biography = ?) WHERE id_author = ? ");
     $sentencia->execute([$id_author, $name, $surname, $nationality, $biography]);
   }
 
-  function borrarAutor($id_author){
+  function deleteAuthor($id_author){
     $sentencia=$this->db->prepare("DELETE FROM author WHERE id_author = ?");
     return $sentencia->execute([$id_author]);
   }
