629f976f04b50087bf14a568202e77b3f66b94d3
Antonela Nespoli <antonela.nespoli@gmail.com>
2018-10-18 17:07:05 -0300

    Se suben archivos book*

---
+++ model/BookModel.php
@@ -0,0 +1,58 @@
+<?php
+
+class BookModel extends Model
+{
+  function getBooks($id_author = null){
+    if($id_author == null){
+    $sentence = $this->db->prepare( "SELECT book.*, a.name AS author FROM book LEFT JOIN author AS a ON a.id_author = book.id_author");
+    $sentence->execute();
+    }else{
+      $sentence = $this->db->prepare( "SELECT book.*, a.name AS author FROM book LEFT JOIN author AS a ON a.id_author = book.id_author WHERE book.id_author = ?");
+      $sentence->execute([$id_author]);
+    } 
+    return $sentence->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  function getBook($id_book){
+    $sentence  = $this->db->prepare( "SELECT book.*, a.name AS author FROM book LEFT JOIN author AS a ON a.id_author = book.id_author WHERE book.id_book = ?");
+    $sentence->execute([$id_book]);
+    return $sentence->fetch();
+  }
+
+  private function addImages($images, $id_book){
+
+    $routes = [];
+
+    foreach ($images as $image) {
+      $route = 'images/' . uniqid() .'.jpg';
+      move_uploaded_file($image, $route);
+      $routes[] = $route;
+    }
+
+    $sentence_images = $this->db->prepare('INSERT INTO [image](id_book, [route]) VALUES(?,?)');
+
+    foreach ($routes as $route) {
+      $sentence_images->execute([$id_book,$route]);
+    }
+  }
+
+  function addBook($name, $gender, $editorial, $id_author, $review, $nbr_pages, $images){
+    $sentence = $this->db->prepare('INSERT INTO book([name], gender, editorial, id_author, review, nbr_pages) VALUES(?,?,?,?,?,?)');
+    $sentence->execute([$name, $gender, $editorial, $id_author, $review, $nbr_pages]);
+    $id_book = $this->db->lastInsertId();
+    $this->addImages($images, $id_book);
+  }
+
+  function editBook($id_book, $name, $gender, $editorial, $id_author, $review, $nbr_pages, $images){
+    $sentence = $this->db->prepare( "UPDATE book SET [name] = ?, gender = ?, editorial = ?, id_author = ?, review = ?, nbr_pages = ? WHERE id_book=?");
+    $sentence->execute([$name, $gender, $editorial, $id_author, $review, $nbr_pages, $id_book]);
+    $this->addImages($images, $id_book);
+  }
+
+  function deleteBook($id_book){
+    $sentence = $this->db->prepare( "DELETE FROM book WHERE id_book=?");
+    $sentence->execute([$id_book]);
+  }
+}
+
+ ?>
--- view/BookView.php
+++ view/BookView.php
@@ -1,4 +1,27 @@
 <?php
-
+class BookView extends View
+{
+  function viewBooks($books){
+    $this->smarty->assign('books', $books);
+    $this->smarty->assign('isLoggedIn', UserModel::isLoggedIn());
+    $this->smarty->display('templates/books.tpl');
+  }
+  function viewBook($book){
+    $this->smarty->assign('book', $book);
+    $this->smarty->display('templates/book.tpl');
+  }
+  function viewFormBook($authors, $book = null, $images = null){
+    $this->smarty->assign('authors', $authors);
+    $this->smarty->assign('isSuperUser', UserModel::isSuperUser());
+    if ($book) {
+      $this->smarty->assign('book', $book);
+      if ($images){
+        $this->smarty->assign('images', $images);
+      }
+    }
+    $this->smarty->display('templates/formBook.tpl');
+  }
+  
+}
 
  ?>
