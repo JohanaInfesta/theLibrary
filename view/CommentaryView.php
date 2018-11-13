9c70f2bba9aab86810c013d75c33338d0696425d
Johana Infesta <johana.infesta@gmail.com>
2018-11-13 19:11:12 -0300

    arreglando api para no enloquecer

---
+++ view/CommentaryView.php
@@ -0,0 +1,13 @@
+<?php
+class CommentaryView extends View
+{
+  function mostrarComentarios($comentarios, $usuarios){
+// revisar
+    // $this->smarty->assign('comentarios', $comentarios);
+    // $this->smarty->assign('usuarios', $usuarios);
+    // $this->smarty->assign('isLoggedIn', UsuarioModel::isLoggedIn());
+    // $this->smarty->assign('isSuperUser', UsuarioModel::isSuperUser());
+    // $this->smarty->display('templates/comentarios.tpl');
+  }
+}
+?>
